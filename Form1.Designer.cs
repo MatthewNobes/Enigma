d0974a16a4d1724921d2f803cc4ceda621cc1187
MatthewNobes <mattnobes.personal@protonmail.com>
2020-05-13 18:56:26 +0100

    Basic Setup.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.Text = "Enigma";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
